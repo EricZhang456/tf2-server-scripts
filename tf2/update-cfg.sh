e2b7b76ee4a2a5f5f4c3b697bc07642832c7a325
Eric Zhang <ericxiang456@gmail.com>
2025-03-14 15:55:12 +0800

    add script to update cfg

---
+++ tf2/update-cfg.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/replay.cfg > /home/tf2/tf2/tf/cfg/replay.cfg
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/server.cfg > /home/tf2/tf2/tf/cfg/server.cfg
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/motd.txt > /home/tf2/tf2/tf/cfg/motd.txt
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/mapcycle.txt > /home/tf2/tf2/tf/cfg/mapcycle.txt
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/motd.txt > /home/tf2/tf2/tf/cfg/motd_text.txt
