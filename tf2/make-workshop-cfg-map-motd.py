127ee50643614572e2782aa810dc4371461b08d4
Eric Zhang <ericxiang456@gmail.com>
2025-03-14 18:19:16 +0800

    add script to generate motd map rotation and workshop map update cfg

---
+++ tf2/make-workshop-cfg-map-motd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+with open(sys.argv[1], 'r') as file:
+    for line in file:
+        if (int(sys.argv[2]) == 2):
+            print("tf_workshop_map_sync ", end="")
+        else:
+            print("- ", end="")
+        print(re.search(r'workshop/(.*).ugc([0-9]+)', line).group(int(sys.argv[2])))
