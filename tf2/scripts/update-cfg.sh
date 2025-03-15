4962435424b38fdc26e0eaa8f25c1407863dff9c
Eric Zhang <ericzhang456@disroot.org>
2025-03-15 10:22:00 +0800

    move everything to folders

---
+++ tf2/scripts/update-cfg.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/cfg/replay.cfg > /home/tf2/tf2/tf/cfg/replay.cfg
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/cfg/server.cfg > /home/tf2/tf2/tf/cfg/server.cfg
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/cfg/motd.txt > /home/tf2/tf2/tf/cfg/motd.txt
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/cfg/mapcycle.txt > /home/tf2/tf2/tf/cfg/mapcycle.txt
+curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/cfg/motd.txt > /home/tf2/tf2/tf/cfg/motd_text.txt
+#/home/tf2/scripts/make-workshop-cfg-map-motd.py /home/tf2/tf2/tf/cfg/mapcycle.txt 2 > /home/tf2/tf2/tf/cfg/workshop_update.cfg
+#/home/tf2/scripts/make-workshop-cfg-map-motd.py /home/tf2/tf2/tf/cfg/mapcycle.txt 1 >> /home/tf2/tf2/tf/cfg/motd.txt
+#/home/tf2/scripts/make-workshop-cfg-map-motd.py /home/tf2/tf2/tf/cfg/mapcycle.txt 1 >> /home/tf2/tf2/tf/cfg/motd_text.txt
+/home/tf2/scripts/make-motd.sh /home/tf2/tf2/tf/cfg/mapcycle.txt >> /home/tf2/tf2/tf/cfg/motd.txt
+/home/tf2/scripts/make-motd.sh /home/tf2/tf2/tf/cfg/mapcycle.txt >> /home/tf2/tf2/tf/cfg/motd_text.txt
