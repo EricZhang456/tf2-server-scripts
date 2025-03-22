77e95a8c8e07574571cd7cb434571c3a131eb2ab
Eric Zhang <ericzhang456@disroot.org>
2025-03-22 19:56:30 +0800

    add maps & update motd

--- tf2/scripts/update-cfg.sh
+++ tf2/scripts/update-cfg.sh
@@ -10,5 +10,5 @@ sed -i -e '$a\' /home/tf2/tf2/tf/cfg/mapcycle.txt
 #/home/tf2/scripts/make-workshop-cfg-map-motd.py /home/tf2/tf2/tf/cfg/mapcycle.txt 2 > /home/tf2/tf2/tf/cfg/workshop_update.cfg
 #/home/tf2/scripts/make-workshop-cfg-map-motd.py /home/tf2/tf2/tf/cfg/mapcycle.txt 1 >> /home/tf2/tf2/tf/cfg/motd.txt
 #/home/tf2/scripts/make-workshop-cfg-map-motd.py /home/tf2/tf2/tf/cfg/mapcycle.txt 1 >> /home/tf2/tf2/tf/cfg/motd_text.txt
-/home/tf2/scripts/make-motd.sh /home/tf2/tf2/tf/cfg/mapcycle.txt >> /home/tf2/tf2/tf/cfg/motd.txt
-/home/tf2/scripts/make-motd.sh /home/tf2/tf2/tf/cfg/mapcycle.txt >> /home/tf2/tf2/tf/cfg/motd_text.txt
+#/home/tf2/scripts/make-motd.sh /home/tf2/tf2/tf/cfg/mapcycle.txt >> /home/tf2/tf2/tf/cfg/motd.txt
+#/home/tf2/scripts/make-motd.sh /home/tf2/tf2/tf/cfg/mapcycle.txt >> /home/tf2/tf2/tf/cfg/motd_text.txt
