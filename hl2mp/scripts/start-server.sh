432ef1104a908953d2bea90a1399032e58f5f78b
Eric Zhang <ericxiang456@gmail.com>
2025-05-10 23:26:27 +0800

    fix startup script

--- hl2mp/scripts/start-server.sh
+++ hl2mp/scripts/start-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/hl2mp/hl2mp/srcds_run -game hl2mp -secure -port 27021 +randommap +maxplayers 16 +servercfgfile server.cfg -steam_dir /usr/games/ -steamcmd_script /home/hl2mp/scripts/steamcmd-script.txt -autoupdate
+/home/hl2mp/hl2mp/srcds_run -game hl2mp -secure -port 27021 +map dm_overwatch +maxplayers 16 +servercfgfile server.cfg -steam_dir /usr/games/ -steamcmd_script /home/hl2mp/scripts/steamcmd-script.txt -autoupdate
