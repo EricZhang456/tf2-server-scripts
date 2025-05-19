33c2cb1a9a7e8815e063e6b4cf1a110b95abb89b
Eric Zhang <ericzhang456@disroot.org>
2025-05-19 20:10:50 +0800

    update package name

--- tf2/scripts/update-reverts.sh
+++ tf2/scripts/update-reverts.sh
@@ -2,7 +2,7 @@
 
 mkdir tmp
 cd tmp
-wget -O artifact.zip https://nightly.link/rsedxcftvgyhbujnkiqwe/castaway-plugins/workflows/build/master/artifact.zip
+wget -O artifact.zip https://nightly.link/rsedxcftvgyhbujnkiqwe/castaway-plugins/workflows/build/master/package-1.12.zip
 unzip artifact.zip
 mv -v gamedata/* /home/tf2/tf2/tf/addons/sourcemod/gamedata/
 mv -v plugins/reverts.smx /home/tf2/tf2/tf/addons/sourcemod/plugins/
