4b4ddabeb4ce5fe4a298cbd93e9ac757c1439002
Eric Zhang <ericzhang456@disroot.org>
2025-06-21 13:15:22 +0800

    Revert "increase max players"
    This reverts commit 3b9cef849637cfc9448beb78b86b42deb38a7d2f.

--- tf2/scripts/start-server.sh
+++ tf2/scripts/start-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/tf2/tf2/srcds_run -game tf -secure -port 27015 -maxplayers 32 +randommap +servercfgfile server.cfg +mapcyclefile mapcycle.txt +sv_setsteamaccount $STEAM_SERVER_TOKEN -steam_dir /usr/games/ -steamcmd_script /home/tf2/scripts/steamcmd-script.txt -autoupdate -replay
+/home/tf2/tf2/srcds_run -game tf -secure -port 27015 -maxplayers 24 +randommap +servercfgfile server.cfg +mapcyclefile mapcycle.txt +sv_setsteamaccount $STEAM_SERVER_TOKEN -steam_dir /usr/games/ -steamcmd_script /home/tf2/scripts/steamcmd-script.txt -autoupdate -replay
