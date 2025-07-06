31f48a628e74790db851b546bc4c94312e132eaf
Eric Zhang <ericzhang456@disroot.org>
2025-07-06 16:08:24 +0800

    update metamod

--- l4d2/scripts/update-metamod.sh
+++ l4d2/scripts/update-metamod.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-LATEST_METAMOD=$(curl https://mms.alliedmods.net/mmsdrop/1.11/mmsource-latest-linux)
+LATEST_METAMOD=$(curl https://mms.alliedmods.net/mmsdrop/1.12/mmsource-latest-linux)
 
 mkdir tmp
 
 cd tmp
-wget https://mms.alliedmods.net/mmsdrop/1.11/$LATEST_METAMOD
+wget https://mms.alliedmods.net/mmsdrop/1.12/$LATEST_METAMOD
 mkdir mm
 cd mm
 tar xvf ../$LATEST_METAMOD
