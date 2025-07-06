04d5466dfc7002389055ddf9d30c432572182e79
Eric Zhang <ericzhang456@disroot.org>
2025-07-06 14:02:37 +0800

    fix port

--- css/scripts/start-server.sh
+++ css/scripts/start-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/css/css/srcds_run -game cstrike -secure -port 27022 +map de_dust2 +maxplayers 32 +sv_setsteamaccount $STEAM_SERVER_TOKEN +servercfgfile server.cfg -steam_dir /usr/games/ -steamcmd_script /home/css/scripts/steamcmd-script.txt -autoupdate
+/home/css/css/srcds_run -game cstrike -secure -port 27023 +map de_dust2 +maxplayers 32 +sv_setsteamaccount $STEAM_SERVER_TOKEN +servercfgfile server.cfg -steam_dir /usr/games/ -steamcmd_script /home/css/scripts/steamcmd-script.txt -autoupdate
