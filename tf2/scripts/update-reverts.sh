dc37fa961175008e1a2fc729aad00ea1dbfa7d79
Eric Zhang <ericzhang456@disroot.org>
2026-05-15 13:03:47 +0800

    update reverts update script

--- tf2/scripts/update-reverts.sh
+++ tf2/scripts/update-reverts.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+set -e
+
 mkdir tmp
 cd tmp
-wget -O artifact.zip https://nightly.link/rsedxcftvgyhbujnkiqwe/castaway-plugins/workflows/build/master/package-1.12.zip
+
+LATEST_REVERTS_DOWNLOAD=$(curl "https://api.github.com/repos/rsedxcftvgyhbujnkiqwe/castaway-plugins/releases?per_page=5" | jq -r '.[0].assets[] | select(.name == "package-1.12.zip") | .browser_download_url')
+
+wget -O artifact.zip $LATEST_REVERTS_DOWNLOAD
 unzip artifact.zip
-mv -v gamedata/* /home/tf2/tf2/tf/addons/sourcemod/gamedata/
+mv -v gamedata/reverts.txt /home/tf2/tf2/tf/addons/sourcemod/gamedata/reverts.txt
+mv -v gamedata/memorypatch_reverts.txt /home/tf2/tf2/tf/addons/sourcemod/gamedata/memorypatch_reverts.txt
 mv -v plugins/reverts.smx /home/tf2/tf2/tf/addons/sourcemod/plugins/
-rsync -va translations/* /home/tf2/tf2/tf/addons/sourcemod/translations/
+rsync -va --include="reverts.phrases*" --exclude="*" translations/* /home/tf2/tf2/tf/addons/sourcemod/translations/
 cd ..
+
 rm -r tmp
-curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/cfg/reverts.cfg > /home/tf2/tf2/tf/cfg/sourcemod/reverts.cfg
+
+# curl https://raw.githubusercontent.com/EricZhang456/tf2-server-scripts/refs/heads/master/tf2/cfg/reverts.cfg > /home/tf2/tf2/tf/cfg/sourcemod/reverts.cfg
